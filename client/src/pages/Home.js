7ad47d169455727e09fe63cd14430ed539a127f8
razineAl <razineal@gmail.com>
2024-06-29 23:35:58 +0100

    improvements

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,7 @@
 import {useContext, useEffect, useState, useCallback} from 'react';
 import axios from 'axios';
-import Navbar from '../components/Navbar';
 import AuthContext from '../helpers/AuthContext';
-import { Link, BrowserRouter as Router,Routes,Route, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import Rankings from './Rankings';
 
