bd26c5a088980b3e2e3cd80e0c18ae6d09a17054
razineAl <razineal@gmail.com>
2024-06-29 23:08:30 +0100

    role middleware improvement

--- server/middlewares/roleMiddleware.js
+++ server/middlewares/roleMiddleware.js
@@ -2,7 +2,7 @@ const validateRole = (role)=>{
     return (req,res,next)=>{
         if(role.includes(req.user.role)) next();
         else {
-            return res.status(401).json({error:"you don't have the permissions :/"});
+            return res.status(403).json({error:"you don't have the permissions :/"});
         }
     }
 }
