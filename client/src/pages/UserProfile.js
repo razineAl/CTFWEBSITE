86a3640dd2fbd1c9c43e17a157e7298c740a96dc
razineAl <razineal@gmail.com>
2024-07-05 23:01:18 +0100

    .

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -2,23 +2,35 @@ import {useContext, useEffect, useState} from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import AuthContext from '../helpers/AuthContext';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate, useParams } from 'react-router-dom';
 import Cookies from 'universal-cookie';
 import Home from './Home';
 
 
-function Rankings(){
-    const [topUsers,setTopUsers] = useState([]);
+function UserProfile(){
+    const [user,setUser] = useState({});
+    const {authState,setAuthState} = useContext(AuthContext);
 
 
+    const cookies = new Cookies();
 
     let navigate = useNavigate();
+    let {id} = useParams();
+
 
     useEffect(()=>{
-        axios.get('http://localhost:3001/users/top/10')
+
+        const refreshToken = cookies.get('refreshToken');
+        axios.get('http://localhost:3001/refresh',{headers:{refreshToken:refreshToken}})
         .then((res)=>{
-            setTopUsers(res.data);
-        })
+            setAuthState({username:res.data.username,status:true,accessToken:res.data.accessToken,id:res.data.id,role:res.data.role});
+            axios.get(`http://localhost:3001/users/byId/${id}`)
+            .then((response)=>{
+                setUser(response.data);   
+            })
+        })    
+
+        
     },[])
 
     return(
@@ -29,4 +41,4 @@ function Rankings(){
     )
 }
 
-export default Rankings;
+export default UserProfile;
