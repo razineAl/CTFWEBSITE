75ec472ea92c45f71328dd28f1ce9be05dd2a7f2
razineAl <razineal@gmail.com>
2024-07-05 22:14:30 +0100

    wraping the try catch block around these

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,15 +3,15 @@ const {verify} = require('jsonwebtoken');
 
 const validateToken = async (req,res,next)=>{
 
-    const accessToken = req.header('accessToken');
-
-    if(!accessToken) return res.status(403).json({error:'user not logged in !'});
-
-    const validToken = verify(accessToken,process.env.ACCESS_TOKEN_SECRET);
-
-    req.user = validToken;
 
     try {
+        const accessToken = req.header('accessToken');
+
+        if(!accessToken) return res.status(403).json({error:'user not logged in !'});
+
+        const validToken = verify(accessToken,process.env.ACCESS_TOKEN_SECRET);
+
+        req.user = validToken;
         if (validToken) return next();
     } catch (error) {
         res.sendStatus(401);
