a7461c9794d24cdc9d630a0ca000272bde1516ee
razineAl <razineal@gmail.com>
2024-07-09 21:01:45 +0100

    .

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,11 +5,11 @@ const Challenge = require('../model/Challenge');
 const validateToken = require('../middlewares/authMiddleware');
 const {validateRole} = require('../middlewares/roleMiddleware');
 
-router.post('/createChallenge',validateToken,validateRole('admin'),async (req,res)=>{ //creation d'un nouveau challenge
+router.post('/createChallenge',validateToken,validateRole('admin'),async (req,res)=>{ 
     const challenge = await Challenge.create(req.body);
     res.json('success');
 });
-router.put('/updateChallenge',validateToken,validateRole('admin'),async (req,res)=>{ //modification d'un challenge
+router.put('/updateChallenge',validateToken,validateRole('admin'),async (req,res)=>{ 
     const challenge = await Challenge.create(req.body);
     res.json('success');
 });
