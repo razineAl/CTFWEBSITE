9da54ec33cb18f328fb0d36862b34c7ca4c2667d
razineAl <razineal@gmail.com>
2024-07-11 22:49:14 +0100

    new component

---
+++ client/src/components/DifficultySpan.js
@@ -0,0 +1,14 @@
+function DifficultySpan() {
+
+  return (
+
+            <div className="difficulty-container">
+                <span className={`difficulty-1`}></span>
+                <span className={`difficulty-2`}></span>
+                <span className={`difficulty-3`}></span>
+                <span className={`difficulty-4`}></span>
+                <span className={`difficulty-5`}></span>
+            </div>
+  );
+}
+export default DifficultySpan;
