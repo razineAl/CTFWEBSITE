530012b65f917bfbc5445c5497481f585f4f7469
razineAl <razineal@gmail.com>
2024-07-24 23:55:15 +0100

    footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import image from '../Elmore.png';
+
+
+function Footer() {
+  return (
+    <footer id='main-footer'>
+        <div className="footer-container">
+            <div className="footer-logo">
+                <img src={image} alt="Website Logo"/>
+            </div>
+            <div className="footer-content">
+                <p>&copy; 2024 Elmore CTF. All rights reserved.</p>
+                <ul className="footer-links">
+                  <li><a href="/about">About Us</a></li>
+                  <li><a href="/contact">Contact</a></li>
+                  <li><a href="/privacy">Privacy Policy</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
