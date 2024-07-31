7df892cff94579a40f3d3c09f74e2170fdea4a7a
razineAl <razineal@gmail.com>
2024-07-31 19:38:29 +0100

    some slight changes to the barChart component

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -12,14 +12,13 @@ const BarChart = ({ data }) => {
       tooltip: {
         callbacks: {
           label: function(context) {
-            // Custom tooltip label
             return `${context.dataset.label}: ${context.parsed.y}`;
           }
         },
-        backgroundColor: '#000', // Tooltip background color
-        titleColor: '#fff', // Tooltip title color
-        bodyColor: '#fff', // Tooltip body color
-        borderColor: '#fff', // Tooltip border color
+        backgroundColor: '#000', 
+        titleColor: '#fff', 
+        bodyColor: '#fff', 
+        borderColor: '#fff', 
         borderWidth: 1
       }
     },
@@ -29,7 +28,7 @@ const BarChart = ({ data }) => {
           display: false,
         },
         ticks: {
-          color: '#000' // X-axis labels color
+          color: '#000' 
         }
       },
       y: {
@@ -38,7 +37,7 @@ const BarChart = ({ data }) => {
         },
         ticks: {
           beginAtZero: true,
-          color: '#000' // Y-axis labels color
+          color: '#000' 
         }
       }
     }
@@ -48,18 +47,18 @@ const BarChart = ({ data }) => {
     labels: data.labels,
     datasets: [
       {
-        label: 'Solved Challenges',
+        label: 'Solved',
         data: data.values,
         backgroundColor: [
-          '#FF6384',
           '#36A2EB',
+          '#FF6384',
           '#FFCE56',
           '#4BC0C0',
           '#9966FF',
           '#FF9F40'
         ],
-        borderRadius: 2, // Bar corners radius
-        barThickness: 10, // Bar width
+        borderRadius: 2, 
+        barThickness: 10, 
       }
     ]
   };
