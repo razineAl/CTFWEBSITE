de5b6f87ac35e62bcbf1db4e570356675c4ac413
razineAl <razineal@gmail.com>
2024-08-27 23:29:43 +0100

    adding some routes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,10 @@ import Admin from "./pages/Admin";
 import UserProfile from "./pages/UserProfile";
 import Challenges from "./pages/Challenges";
 import ChallengePage from "./pages/ChallengePage";
+import Premium from "./pages/Premium";
+import FAQ from "./pages/FAQ";
+import Registration from "./pages/Registration";
+import Settings from "./pages/Settings";
 
 function App() {
   const [authState,setAuthState] = useState({username:'',status:false,accessToken:'',id:'',role:'user'});
@@ -23,9 +27,13 @@ function App() {
             <Route path="/home" exact Component={Home}></Route>
             <Route path="/challenges" exact Component={Challenges}></Route>
             <Route path="/ranking" exact Component={Rankings}></Route>
+            <Route path="/billing" exact Component={Premium}></Route>
             <Route path="/admin-panel" exact Component={Admin}></Route>
             <Route path="/profile/:id" exact Component={UserProfile}></Route>
             <Route path="/challenge/:id" exact Component={ChallengePage}></Route>
+            <Route path="/faq" exact Component={FAQ}></Route>
+            <Route path="/registration" exact Component={Registration}></Route>
+            <Route path="/account/settings/:id" exact Component={Settings}></Route>
             
 
           </Routes>
