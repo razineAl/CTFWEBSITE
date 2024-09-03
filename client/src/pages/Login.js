40977ca1069ce30fd65a9f2ac8f0b99ce130f7c2
razineAl <razineal@gmail.com>
2024-09-03 23:31:38 +0100

    redesigning the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,9 @@
-
 import Navbar from '../components/Navbar';
 import {useContext, useEffect, useState} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import AuthContext from '../helpers/AuthContext';
-import Cookies from 'universal-cookie';
+import Footer from '../components/Footer';
+
 
 
 
@@ -13,7 +12,7 @@ function Login(){
     const [password,setPassword] = useState('');
     const [focused,setFocused] = useState(false);
     const [focus,setFocus] = useState(false);
-    const {authState,setAuthState} = useContext(AuthContext);
+
 
     let navigate = useNavigate();
 
@@ -56,37 +55,35 @@ function Login(){
     return(
         <div id='login-page'>
 
-        
             <Navbar></Navbar>
-            <div id='title'>
-                <span style={{animationDelay:0.1+'s'}}>E</span>
-                <span style={{animationDelay:0.2+'s'}}>L</span>
-                <span style={{animationDelay:0.3+'s'}}>M</span>
-                <span style={{animationDelay:0.4+'s'}}>O</span>
-                <span style={{animationDelay:0.5+'s'}}>R</span>
-                <span style={{animationDelay:0.6+'s'}}>E</span>
-                <br></br>
-                <span style={{animationDelay:0.7+'s'}}>C</span>
-                <span style={{animationDelay:0.8+'s'}}>T</span>
-                <span style={{animationDelay:0.9+'s'}}>F</span>
-            </div>
-            <form onSubmit={(e)=>{
-                e.preventDefault();
-                if (email!=='' && password!=='') {
-                    sendLogin();
-                }
+            <main id='login-page-main-content'>
+                <div className='content'>
+                    <h2>Elmore</h2>
+                    <h2>Elmore</h2>
+                </div>
+                <div id='login-form-container'>
+                <form onSubmit={(e)=>{
+                    e.preventDefault();
+                    if (email!=='' && password!=='') {
+                        sendLogin();
+                    }
                 }}>
 
-                <div>
-                    <label htmlFor='user' className={focus ? 'focused' : ''}>Email</label>
-                    <input className={ focus ? 'focused' : ''} type='text' id='user' name='user' value={email} onBlur={handleBlur} onFocus={handleFocus} onChange={(e)=>{setEmail(e.target.value)}}></input>
+                    <div>
+                        <label htmlFor='user' className={focus ? 'focused' : ''}>Email</label>
+                        <input className={ focus ? 'focused' : ''} type='text' id='user' name='user' value={email} onBlur={handleBlur} onFocus={handleFocus} onChange={(e)=>{setEmail(e.target.value)}}></input>
+                    </div>
+                    <div>
+                        <label htmlFor='pwd' className={focused ? 'focused' : ''}>Password</label>
+                        <input className={focused ? 'focused' : ''} type='password' id='pwd' name='pwd' value={password} onBlur={handleBlured} onFocus={handleFocused} onChange={(e)=>{setPassword(e.target.value)}}></input>
+                    </div>
+                    <button type='submit' className='bg-blue'>Log in</button>
+                </form>
                 </div>
-                <div>
-                    <label htmlFor='pwd' className={focused ? 'focused' : ''}>Password</label>
-                    <input className={focused ? 'focused' : ''} type='password' id='pwd' name='pwd' value={password} onBlur={handleBlured} onFocus={handleFocused} onChange={(e)=>{setPassword(e.target.value)}}></input>
-                </div>
-                <button type='submit' className='bg-blue'>Log in</button>
-            </form>
+                
+            </main>
+            <Footer></Footer>
+            
             
         </div>
         
